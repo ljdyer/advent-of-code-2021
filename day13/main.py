bb4abfc602b29e689694ce74c1b1ae370b68d2ed
ljdyer <74581995+ljdyer@users.noreply.github.com>
2021-12-13 07:36:06 +0000

    day13 (WIP)

---
+++ day13/main.py
@@ -0,0 +1,111 @@
+from helper import *
+from tabulate import tabulate
+
+# Get data
+lines = get_lines_from_file("test.txt")
+# lines = get_lines_from_file("data.txt")
+
+lines = [l.split('-') for l in lines]
+blank_line = lines.index([''])
+dots = lines[:blank_line]
+
+dots = [l[0].split(',') for l in dots]
+for i in range(len(dots)):
+    dots[i] = tuple((int(dots[i][0]), int(dots[i][1])))
+
+instructions = lines [blank_line+1:]
+
+m_max = max([int(x[0]) for x in dots])
+n_max = max([int(x[1]) for x in dots])
+
+matrix = [['.' for m in range(m_max+1)] for n in range(n_max+1)]
+
+for x,y in dots:
+    matrix[y][x] = '#'
+
+
+def fold_y(y_fold, matrix):
+    lines_above_fold = matrix[:y_fold]
+    lines_below_fold = matrix[-1:y_fold:-1]
+    print(matrix[y_fold])
+    for i in range(len(lines_below_fold)):
+        for y in range(len(lines_above_fold[i])):
+            if lines_below_fold[i][y] == '#':
+                lines_above_fold[i][y] = '#'
+    return lines_above_fold
+
+def fold_x(x_fold, matrix):
+    lines_left_fold = [l[:x_fold] for l in matrix]
+    lines_right_fold = [l[-1:x_fold:-1] for l in matrix]
+    fold_column = [l[x_fold] for l in matrix]
+    print(fold_column)
+
+    # print(tabulate(lines_left_fold))
+    # print(tabulate(lines_right_fold))
+    for row in range(len(lines_right_fold)):
+        for column in range(len(lines_left_fold[row])):
+            if lines_right_fold[row][column] == '#':
+                lines_left_fold[row][column] = '#'
+    return lines_left_fold
+
+def print_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end='')
+        print()
+
+# y_fold = int(instructions[0][0].partition('=')[2])
+# matrix = fold_y(y_fold, matrix)
+
+matrix = fold_x(5,matrix)
+matrix = fold_y(7, matrix)
+# matrix = fold_x(2, matrix)
+print_matrix(matrix)
+
+
+# print(tabulate(matrix))
+
+all = [i for x in matrix for i in x if i == '#']
+print(len(all))
+# print(tabulate(lines_below_fold))
+
+# # x_fold = int(instructions[1][0].partition('=')[2])
+# print(y_fold)
+
+# num_put = 0
+# y_bottom = n_max
+# # for x in range(y_bottom, y_fold,-1):
+# #     # print(x)
+# #     for y in range(len(matrix[x])):
+# #         # print(y)
+# #         if matrix[x][y] == '#':
+# #             new_x = y_fold - (x - y_fold)
+# #             print(x,y,new_x,y)
+# #             num_put += 1
+# #             if matrix[new_x][y] == '#':
+# #                 print('overlapping')
+
+
+# # new_x = 7 - (14 - 14)
+
+# # # x_bottom = x_fold*2 + 1
+# # # for y in range(x_bottom, x_fold,-1):
+# # #     print(y)
+# # #     for x in range(len(matrix)):
+# # #         if matrix[x][y] == '#':
+# # #             new_y = x_bottom - y - 1
+# # #             print(x,y,new_x,y)
+# # #             matrix[x][new_y] = '#'
+
+# # # # 15 -> 1
+# # # # 14 -> 2
+# # # # 13 -> 3
+
+    
+# # print(tabulate(matrix[0:2]))
+# visible_lines = [matrix[m] for m in range(y_fold)]
+# invisible_lines = [matrix[m] for m in range(y_fold,y_bottom)]
+# all = [i for x in visible_lines for i in x if i == '#']
+# print(len(all))
+# all = [i for x in invisible_lines for i in x if i == '#']
+# print(len(all))
